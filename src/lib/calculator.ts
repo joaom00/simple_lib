004f7b79a28c7002b621c89b311c76d9b90bd67d
João Pedro Magalhães <joaopsilvamagalhaes@gmail.com>
2022-09-11 17:16:01 -0300

    initial commit

---
+++ src/lib/calculator.ts
@@ -0,0 +1,8 @@
+export function sum(num1?: any, num2?: any) {
+  const int1 = parseInt(num1, 10)
+  const int2 = parseInt(num2, 10)
+
+  if (Number.isNaN(int1) || Number.isNaN(int2)) throw new Error('Please check your input')
+
+  return +int1 + +int2
+}
